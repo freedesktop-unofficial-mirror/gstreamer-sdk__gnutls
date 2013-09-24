06704d0e2bb580b7f1e7cf1d10731c232800e5e9
Sebastian Dröge <slomo@circular-chaos.org>
2013-09-24 22:52:04 +0200

    Initialize global variable to fix static linking

--- lib/verify-tofu.c
+++ lib/verify-tofu.c
@@ -61,7 +61,7 @@ int store_pubkey(const char* db_name, const char* host,
 static int find_config_file(char* file, size_t max_size);
 #define MAX_FILENAME 512
 
-void *_gnutls_file_mutex;
+void *_gnutls_file_mutex = NULL;
 
 struct gnutls_tdb_int default_tdb = {
   store_pubkey,
